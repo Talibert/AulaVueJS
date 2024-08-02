3919a721f82cd2cf47992138bd6f1799a2ca687f
Guilherme Taliberti <guilhermetaliberti@gmail.com>
2024-08-01 21:24:09 -0300

    iniciando o uso de components

---
+++ src/components/HeaderButton.vue
@@ -0,0 +1,25 @@
+<template>
+    <button class="headerButton">
+        Botão teste
+    </button>
+</template>
+
+<script>
+export default {
+    setup() {
+        
+    },
+}
+</script>
+
+<style>
+    .headerButton {
+        background: #000;
+        color: #fff;
+        display: flex;
+        width: fit-content;
+        margin: auto;
+        padding: 3px;
+        font-size: 30px;
+    }
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,31 @@
+<template>
+    <header class="header">
+        <HeaderButton/>
+        <HeaderButton/>
+        <HeaderButton/>
+    </header>
+</template>
+
+<script>
+import HeaderButton from './HeaderButton.vue';
+
+export default {
+    setup() {
+        
+    },
+    components: {
+        HeaderButton
+    }
+}
+</script>
+
+<style>
+    .header {
+        background: #000;
+        color: #fff;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        padding: 3px;
+    }
+</style>
